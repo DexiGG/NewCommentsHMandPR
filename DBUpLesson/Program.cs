ee3af4cc842d769ca92ccb868c81fbe8522042ae
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-04-30 21:15:58 +0600

    NewsComment

---
+++ DBUpLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using DbUp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace DBUpLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringConfiguration = ConfigurationManager.ConnectionStrings["AppConnection"];
+            var connectionString = connectionStringConfiguration.ConnectionString;
+            #region Migration
+            EnsureDatabase.For.SqlDatabase(connectionString);
+            var upgrader =
+                    DeployChanges.To
+                        .SqlDatabase(connectionString)
+                        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                        .LogToConsole()
+                        .Build();
+
+            var result = upgrader.PerformUpgrade();
+            if (!result.Successful) throw new Exception("Миграция не удалась");
+#endregion
+
+        }
+    }
+}
